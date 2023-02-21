4b4136d4cd29050bc8e2fac278945ad2df025b1c
Antont25 <toxafootbol1994@gmail.com>
2023-02-21 19:08:28 +0200

    feature: add libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useTranslation } from 'react-i18next';
+
+import 'App.scss';
+
+const App = () => {
+  const { t, i18n } = useTranslation('header');
+
+  console.log(i18n);
+  const handleChange = (value: string) => {
+    i18n.changeLanguage(value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+        <div onClick={() => handleChange('uk')}>{t('language.uk')}</div>
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+        <div onClick={() => handleChange('en')}>{t('language.en')}</div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
